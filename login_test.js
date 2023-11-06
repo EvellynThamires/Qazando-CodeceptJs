18ff3877dc09c370d1727799c2b89606847c58f9
Evellyn Thamires <xe.uyemura@gmail.com>
2023-11-06 20:40:18 -0300

    adding the first test

---
+++ login_test.js
@@ -0,0 +1,9 @@
+Feature('login');
+
+Scenario('Success login',  ({ I }) => {
+    I.amOnPage('http://automationpratice.com.br');
+});
+
+Scenario('Failure login',  ({ I }) => {
+    I.amOnPage('http://automationpratice.com.br');
+});
