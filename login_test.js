8a272d32af00e05a90a929f76d49b680107e3a14
Evellyn Thamires <xe.uyemura@gmail.com>
2023-11-16 20:43:39 -0300

    adding the sketch of new scenarios

--- login_test.js
+++ login_test.js
@@ -4,6 +4,14 @@ Scenario('Success login',  ({ I }) => {
     I.amOnPage('http://automationpratice.com.br');
 });
 
-Scenario('Failure login',  ({ I }) => {
+Scenario('Trying to log in when typing only the email',  ({ I }) => {
+    I.amOnPage('http://automationpratice.com.br');
+});
+
+Scenario('Trying to log in without typing the email and password',  ({ I }) => {
+    I.amOnPage('http://automationpratice.com.br');
+});
+
+Scenario('Trying to log in when typing only the password',  ({ I }) => {
     I.amOnPage('http://automationpratice.com.br');
 });
