be828c243c74619161a4b95180362e37902582e5
Evellyn Thamires <xe.uyemura@gmail.com>
2023-11-21 23:07:12 -0300

    adding READ.ME and adding login scenarios

--- login_test.js
+++ login_test.js
@@ -2,16 +2,36 @@ Feature('login');
 
 Scenario('Success login',  ({ I }) => {
     I.amOnPage('http://automationpratice.com.br');
-});
+    I.click('Login')
+    I.waitForText('Login', 30)
+    I.fillField('#user', 'e@email.com')
+    I.fillField('#password', '123456')
+    I.click('#btnLogin')
+    I.waitForText('Login realizado', 3)
+}).tag('@success')
 
 Scenario('Trying to log in when typing only the email',  ({ I }) => {
     I.amOnPage('http://automationpratice.com.br');
-});
+    I.click('Login')
+    I.waitForText('Login', 30)
+    I.fillField('#user', 'e@email.com')
+    I.click('#btnLogin')
+    I.waitForText('Senha inválida', 5)
+}).tag('@error')
 
 Scenario('Trying to log in without typing the email and password',  ({ I }) => {
     I.amOnPage('http://automationpratice.com.br');
-});
+    I.click('Login')
+    I.waitForText('Login', 30)
+    I.click('#btnLogin')
+    I.waitForText('E-mail inválido', 5)
+}).tag('@error')
 
 Scenario('Trying to log in when typing only the password',  ({ I }) => {
     I.amOnPage('http://automationpratice.com.br');
-});
+    I.click('Login')
+    I.waitForText('Login', 30)
+    I.fillField('#password', '123456')
+    I.click('#btnLogin')
+    I.waitForText('E-mail inválido', 10)
+}).tag('@error')
